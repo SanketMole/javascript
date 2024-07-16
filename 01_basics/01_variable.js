60ecacc4f8c0fa14338ce9c6a0484a2ceb41440d
SanketMole <114010703+SanketMole@users.noreply.github.com>
2024-07-16 05:40:39 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 15645
+let accountEmail = "ssm@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
