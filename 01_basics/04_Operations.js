30b67fd40fe5d32b14250dffa10192a56282313a
SanketMole <114010703+SanketMole@users.noreply.github.com>
2024-07-17 06:50:02 +0000

    Operation_confusions

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,41 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2**2);
+
+let str1 = "hello"
+let str2 = "Sanket"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);   //considers as string 
+// console.log(1+"2");     // considers as string
+// console.log("1"+2+2);  //considers as string 
+
+// console.log(1+2+"2");  // considers first 2 as integer/number after that as string
+
+// console.log(+true) => true
+// console.log(true+) => error
+// console.log(+"") => false
+// console.log(+"India") => true
+
+// let n = 100 
+// n++;      // similar if ++n
+// console.log(n);
+
+//Contrastly 
+
+// let x = 3, y=4;
+// console.log(x,y)
+
+let x=3
+// const y = x++
+// console.log(`x:${x}, y:${y}`)  // x=4, y=3
+
+// let y = x++
+// console.log(`x:${x}, y:${y}`) // x=4, y=3
+
+let y = ++x;
+console.log(`x:${x} , y:${y}`)  // x=4, y=4
