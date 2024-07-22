54305176de6bbecbf7812d26d5aa23216cae7646
SanketMole <114010703+SanketMole@users.noreply.github.com>
2024-07-22 17:43:39 +0000

    Number_DataType

---
+++ 01_basics/09_nums.js
@@ -0,0 +1,19 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+// console.log(balance.toString())
+
+// console.log(balance.toFixed(3))
+
+const otherNumber = 12.89
+// console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toPrecision(2))
+// console.log(otherNumber.toPrecision(1))
+// console.log(otherNumber.toPrecision(0))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+console.log(hundreds.toLocaleString('en-IN').length)
