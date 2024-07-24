90d34e23f3083062ecf65e7800ea4c2bd5570b3b
SanketMole <114010703+SanketMole@users.noreply.github.com>
2024-07-24 03:54:00 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,17 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+const myHeros = ['jk','pk']
+
+const myArr2 = new Array(1,2,3,4,5,6,7,8,9,0)
+
+// console.log(myArr, myHeros, myArr2)
+
+
+//Array methods
+
+// myArr.push(6,7)
+// myArr.pop()
+// console.log(myArr)
+
