b5f328a187a36bed80af2ea93aba874eacb26f00
SanketMole <114010703+SanketMole@users.noreply.github.com>
2024-07-25 03:43:53 +0000

    Array_and_ArrayMethods

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -13,5 +13,28 @@ const myArr2 = new Array(1,2,3,4,5,6,7,8,9,0)
 
 // myArr.push(6,7)
 // myArr.pop()
-// console.log(myArr)
 
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//Slics and splice
+console.log("A", myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr)
+
+// Splice alters original Array unlike slice
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("C",myArr);
