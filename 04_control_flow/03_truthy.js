428a24a242d308eeb68377848376a47ec7fbc4f0
SanketMole <114010703+SanketMole@users.noreply.github.com>
2024-07-29 17:45:03 +0000

    Nullish_terniary_operator

---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,31 @@
+// const userEmail = 'hi'
+
+// if(userEmail){
+//     console.log("Got user email")
+// } else{
+//     console.log("Don't have user email")
+// }
+
+
+//falsy values
+
+// false, 0, -0, BigInt (0n), null, undefined, NaN 
+
+//thruthy values
+// ture, "0", 'false', " "(space), [], {}, function(){}
+
+
+
+// // Nullish Coalescing Operator (??)
+// let val1 
+// // val1 = 5 ?? 10
+// val1 = null ?? 10
+
+
+// console.log(val1)
+
+
+//Terniary Operator
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less") : console.log("more")
