f1923ce84f1a95d359e047b69a0563a236a8d30a
SanketMole <114010703+SanketMole@users.noreply.github.com>
2024-07-29 17:44:26 +0000

    switch_control_flow

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,49 @@
+// // switch (key) {
+// //     case value:
+        
+// //         break;
+
+// //     default:
+// //         break;
+// // }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("jan")
+//         break;
+//     case 3:
+//         console.log("feb")
+//         break;
+//     case 5:
+//         console.log("Mar")
+//         break;
+//     case 4:
+//         console.log("Apr")
+//         break;    
+        
+
+
+//     default:
+//         console.log("Default case match")
+//         break;
+// }
+
+
+//For String Values
+
+const month = "mar"
+switch(month){
+    case 'jan':
+        console.log("jan")
+        break;
+    case 'mar':
+        console.log('mar')
+        break;
+
+
+    default:
+        console.log("Invalid")
+        break;
+}
+
