42bee9268c773a4792f1297fd09d5cc4619a52b3
Sanket Mole <114010703+SanketMole@users.noreply.github.com>
2025-04-01 18:15:37 +0000

    Types of promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -54,5 +54,30 @@ promiseFour.then((user) => {
 
 
 const promiseFive = new Promise(function(resolve, reject){
-    
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "javascript", password: "123"})
+        }else{
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
 })
+
+async function consumePromiseFive(){
+    const response = await promiseFive
+    console.log(response)
+}
+
+//Handling error gracefully
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive 
+        console.log(response); 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
